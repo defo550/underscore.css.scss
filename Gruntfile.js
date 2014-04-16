7b93f20ba95e3e7d81b2bc078fc04606fbb9cb9f
Chris DeFelice <christopherdefelice@wi-design.com>
2014-04-16 15:07:28 -0400

    chore(gruntfile): Add a sass and watch task for compilation of scss to css files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,14 +10,44 @@ module.exports = function( grunt ) {
 
   grunt.initConfig({
 
+    config: {
+      cssDir: 'css',
+      scssDir: 'scss'
+    },
+
     // Imports the JSON metadata stored in package.json.
     pkg: grunt.file.readJSON('package.json'),
 
     banner : '/**!\n' +
-            '* <%= pkg.name.split("-").join(" ") %> v<%= pkg.version %>\n' +
+            '* <%= pkg.name %> v<%= pkg.version %>\n' +
             '* Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
             '* UnderscoreCSS may be freely distributed under the MIT license.\n' +
             '*/',
+
+    watch: {
+      styles: {
+        files: ['<%= config.scssDir %>/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          livereload: true // needed to run LiveReload
+        }
+      }
+    },
+
+    sass: {
+      dev: {
+        files: {
+          '<%= config.cssDir %>/images.css' : '<%= config.scssDir %>/images.scss',
+          '<%= config.cssDir %>/layout.css' : '<%= config.scssDir %>/layout.scss',
+          '<%= config.cssDir %>/lists.css' : '<%= config.scssDir %>/lists.scss',
+          '<%= config.cssDir %>/offsets.css' : '<%= config.scssDir %>/offsets.scss',
+          '<%= config.cssDir %>/sizing.css' : '<%= config.scssDir %>/sizing.scss',
+          '<%= config.cssDir %>/skins.css' : '<%= config.scssDir %>/skins.scss',
+          '<%= config.cssDir %>/spacing.css' : '<%= config.scssDir %>/spacing.scss',
+          '<%= config.cssDir %>/text.css' : '<%= config.scssDir %>/text.scss'
+        }
+      }
+    },
 
     concat: {
       options: {
@@ -25,8 +55,8 @@ module.exports = function( grunt ) {
         banner: '<%= banner %>\n'+'\n',
       },
       dist: {
-        src: ['stylesheets/*.css'],
-        dest: 'underscore.css',
+        src: ['<%= configcssDir %>/*.css'],
+        dest: '<%= pkg.name.toLowerCase() %>.css',
       }
     },
 
@@ -37,11 +67,15 @@ module.exports = function( grunt ) {
           report: 'gzip'
         },
         files: {
-          'underscore.min.css': ['stylesheets/*.css']
+          '<%= pkg.name.toLowerCase() %>.min.css': ['<%= config.cssDir %>/*.css']
         }
       }
     }
   });
+
+  grunt.registerTask('listen', [
+    'watch'
+  ]);
 
   grunt.registerTask('build', [
     'concat',
